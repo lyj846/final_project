e3cd1311e1235c91e6818bba9b79e0a4aa9c8417
lyj87069 <lyj87069@gmail.com>
2019-01-11 01:45:09 +0800

    add

---
+++ src/YC.Core/Repository/IOpenDataRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using L.Models;
+
+namespace L.Repository
+{
+    public interface IOpenDataRepository
+    {
+        void Delete(int id);
+        void Insert(OpenData item);
+        List<OpenData> SelectAll(string name);
+        void Update(OpenData item);
+    }
+}
